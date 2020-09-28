57986e778a31811bba47991c58e1860e417b9d6f
rekib <rkb.ra0025@gmail.com>
2020-09-29 01:39:13 +0530

    user authentication

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import express from "express";
 import path from "path";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv"
 
+import auth from "./routes/auth";
+
+dotenv.config()
 const app = express();
+app.use(bodyParser.json());
 
-app.post("/api/auth", (req, res) => {
-  res.status(400).json({ errors: { global: "Invalid Credentials" } });
-});
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Database connected");
+  });
+
+app.use("/api/auth", auth);
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
---
+++ src/models/User.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const schema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      index: true,
+    },
+    passwordHash: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+schema.methods.isValidPassword = function isValidPassword(password) {
+  return bcrypt.compareSync(password, this.passwordHash);
+};
+
+
+schema.methods.generateJWT = function generateJWT() {
+  return jwt.sign(
+    {
+      email: this.email,
+      confirmed: this.confirmed
+    },
+    process.env.JWT_SECRET
+  );
+};
+
+
+schema.methods.toAuthJSON = function toAuthJSON() {
+  return {
+    email: this.email,
+    confirmed: this.confirmed,
+    token: this.generateJWT()
+  };
+};
+
+export default mongoose.model("User", schema);
